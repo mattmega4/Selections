7fa553b9c6e5b462f1af11ec0f79c01be8358c8e
Matthew Singleton <mattmega4@gmail.com>
2016-12-24 00:24:26 -0800

    first commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'Selections' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Selections
+  pod 'Firebase/Core'
+
+end
---
+++ Selections/LandingPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LandingPageViewController.swift
+//  Selections
+//
+//  Created by Matthew Singleton on 12/24/16.
+//  Copyright © 2016 Matthew Singleton. All rights reserved.
+//
+
+import UIKit
+
+class LandingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
