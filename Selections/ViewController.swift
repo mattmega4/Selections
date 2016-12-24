78f08000de5de2ebfc2306fd37917fd94c295aac
Matthew Singleton <mattmega4@gmail.com>
2016-12-24 00:16:04 -0800

    Initial Commit

---
+++ Selections/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Selections
+//
+//  Created by Matthew Singleton on 12/24/16.
+//  Copyright © 2016 Matthew Singleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
